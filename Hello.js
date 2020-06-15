d8d1404d133bca606456dc0a5d57d98c81b11312
dloibl <daniel.loibl@gmail.com>
2020-06-15 20:02:27 +0200

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Document, Page } from 'react-pdf'
+
+export default ({ name }) => <h1><Document><Page/></Document></h1>;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2badtp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2badtp)
